bccfb4e586f933d283f9674791c6247f1bf3cea4
tristian o'brien <tristian2@hotmail.com>
2013-11-03 21:37:12 +0000

    first run

---
+++ ConsoleApplication7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ParseHtmlLinks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            String urlToParse = ("http://www.dcs.bbk.ac.uk/~martin/sewn/ls3/");
+            ParseHtmlLinks phl = new ParseHtmlLinks(urlToParse);
+
+            Console.WriteLine("Parsing links from: " + phl.getUrlToParse());
+            phl.parseHtmlLinks(urlToParse);
+
+            Report report = new Report(phl.ReportList);
+            report.CrawlReport = @"C:\Users\Public\Crawl.txt";
+            report.ResultsReport = @"C:\Users\Public\Results.txt";
+            report.generateReport();
+            Console.Write("Report run, check \n" + report.CrawlReport + " and " + report.ResultsReport + "\nPress any key to exit");
+            Console.Read();
+        }
+    }
+}
